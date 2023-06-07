115b291292a72d5ce721073b1578ac39a0cdf15e
Manoella Waideman <mherreriaswaideman@gmail.com>
2023-06-07 14:05:25 -0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,14 +1,37 @@
-void setup() {
+# include  <LiquidCrystal.h>
+ 
+
+int balanca;
+int peso;
+int led = 13;
+
+LiquidCrystal  lcd  (7, 8, 9, 10, 11, 12); 
+
+void setup()
+{
+  lcd.begin(16, 2);        
+  lcd.setCursor(0, 0);      
+  lcd.setCursor (0,1);
+  lcd.print ("Alimento");
   Serial.begin(9600);
+  delay (2000);
+  lcd.clear();
 }
-
-void loop() {
-  int sensorValue = analogRead(A0); // Ler o valor analógico do pino A0
-  float peso = map(sensorValue, 0, 1023, 0, 100); // Mapear o valor do potenciômetro para uma faixa de peso
-
-  Serial.print("Peso doado: ");
-  Serial.print(peso);
-  Serial.println(" kg");
-
-  delay(1000);
+ 
+void loop()
+{  
+  pinMode(led, OUTPUT);
+  
+  lcd.setCursor(0, 1);             
+  lcd.print ("peso: ");
+  lcd.print(peso);  
+  lcd.print ("g"); 
+  
+  balanca=analogRead(0);
+  peso=map(balanca, 0, 914, 0, 30000);
+  
+  
+    
+  if (peso > 750) digitalWrite(led, HIGH);
+  if (peso < 50) digitalWrite(led, LOW);;
 }
